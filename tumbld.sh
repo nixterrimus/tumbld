1e884ca624cf1ee2c289d15e5c14639060550921
Parker Moore <parkrmoore@gmail.com>
2012-12-06 22:59:32 -0500

    Handling `-s` arg to put into subfolders

--- tumbld.sh
+++ tumbld.sh
@@ -2,11 +2,13 @@
 
 download_site()
 {
-	# Go to subfolder for organized downloading
-	if [ ! -d "$site" ]; then
-		mkdir $site
-	fi
-	cd $site
+  if [ $USE_SUBFOLDERS ]; then
+	  # Go to subfolder for organized downloading
+	  if [ ! -d "$site" ]; then
+		  mkdir $site
+	  fi
+	  cd $site
+  fi
 	
 	# Download the images using wget
 	wget --quiet -H -Dmedia.tumblr.com,$site.tumblr.com -r -R "*avatar*" -A "[0-9]" \
@@ -16,33 +18,62 @@ download_site()
 	# Clean up pages needed to find images
 	rm -f 1 2 3 4 5 6 7 8 9 index.html
 	
-	cd ../
+  if [ $USE_SUBFOLDERS ]; then
+    cd ../
+  fi
 }
 
 usage()
 {
-	echo "download images from tumblr"
-  echo "Usage: `basename $0` [-s] file"
-  exit $E_BADARGS
+  cat << EOF
+  usage: $0 [options] file_or_site_name
+
+  download images from tumblr
+
+  OPTIONS:
+     -h      Show this message
+     -s      Put downloaded images in subfolders
+EOF
 }
 
-if [ $# -ne 1 ]
+# Ensure that there are args
+if [ $# -lt 1 ]
 then
-	echo "download images from tumblr"
-  echo "Usage: `basename $0` file"
+  usage
   exit $E_BADARGS
 fi
 
-if [ -e $1 ]
+USE_SUBFOLDERS=0
+Sites=()
+
+# Parse args
+for var in "$@"
+do
+  if [ "$var" == "-s" ]; then
+    USE_SUBFOLDERS=1
+  else
+    Sites=("${Sites[@]}" "$var")
+  fi
+done
+
+# Ensure that a site or file of sites has been set
+if [ ${#Sites[@]} -lt 1 ]
 then
-	# Download a site of tumblrs using a file as source
-	cat $1 | while read site; do
-			if [[ $site != \#* ]]; then
-				download_site $site
-			fi
-	done
+  usage
+  exit $E_BADARGS
+fi
+
+# Act accordingly
+if [ -e ${Sites[0]} ]
+then
+  # Download a site of tumblrs using a file as source
+  cat ${Sites[0]} | while read site; do
+    if [[ $site != \#* ]]; then
+      download_site $site
+    fi
+  done
 else
-	# download a single tumblr
-	site=$1
-	download_site $site
+  # download a single tumblr
+  site=${Sites[0]}
+  download_site $site
 fi
