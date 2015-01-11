f6c949980e31b02823ad7ecef29344bbb2d26675
Ondrej Brablc <ondrej@brablc.com>
2015-01-11 23:30:22 +0100

    Fix deleting of files and add some statistics.

--- tumbld.sh
+++ tumbld.sh
@@ -13,26 +13,47 @@ download_site()
         pushd $SITE &>/dev/null
     fi
 
+    IMAGE_EXT_RE="jpeg|jpg|bmp|gif|png"
     PAGES_RE=""
     if [[ $PAGES >0 ]]; then
         PAGES_RE="($(seq -s \| 1 ${PAGES})DUMMY)"
     fi
 
+    FILES=()
+    IMG_SIZE=0
+    IMG_COUNT=0
+
     # Download the images using wget
-    wget -H \
-        --domains=media.tumblr.com,$SITE.tumblr.com \
-        --recursive \
-        --reject "*avatar*" \
-        --accept-regex "index|page/${PAGES_RE}|jpeg|jpg|bmp|gif|png" \
-        --level=$PAGES \
-        --no-directories \
-        --no-clobber \
-        --no-verbose \
-        -erobots=off \
-        http://$SITE.tumblr.com/
+    while read LINE; do
+        if [[ $LINE =~ \[([[:digit:]]+)(/[[:digit:]]+)?\][[:space:]]-\>[[:space:]]\"([^\"]+)\" ]]; then
+            echo $LINE
+            SIZE=${BASH_REMATCH[1]}
+            FILE=${BASH_REMATCH[3]}
+            if [[ ! $FILE =~ \.(${IMAGE_EXT_RE})$ ]]; then
+                FILES+=($FILE)
+            else
+                let IMG_SIZE+=$SIZE
+                let IMG_COUNT+=1
+            fi
+        fi
+    done < <(
+        wget -H \
+            --domains=media.tumblr.com,$SITE.tumblr.com \
+            --recursive \
+            --reject "*avatar*" \
+            --accept-regex "index|page/${PAGES_RE}|${IMAGE_EXT_RE}" \
+            --level=$PAGES \
+            --no-directories \
+            --no-clobber \
+            --no-verbose \
+            -erobots=off \
+            http://$SITE.tumblr.com/ 2>&1
+    )
 
     # Clean up pages needed to find images
-    rm -f index.html $(seq -s ' '  1 ${PAGES})
+    echo "Deleting crawling ${#FILES[@]} files ..."
+    rm -f ${FILES[*]}
+    echo "Downloaded ${IMG_COUNT} images with total size ${IMG_SIZE}."
 
     if [[ $USE_SUBFOLDERS == 1 ]]; then
         popd &>/dev/null
@@ -42,17 +63,17 @@ download_site()
 usage()
 {
     cat >&2 <<- EOF
-		usage: $0 [options] [site_name ...]
+        usage: $0 [options] [site_name ...]
 
-		download images from tumblr
+        download images from tumblr
 
-		OPTIONS:
-		-h        Show this message
-		-s        Put downloaded images in subfolders
-		-p PAGES  Number of pages to download (default 5, use 0 for no limits)
-		-l LIST   File with number of sites to download
+        OPTIONS:
+        -h        Show this message
+        -s        Put downloaded images in subfolders
+        -p PAGES  Number of pages to download (default 5, use 0 for no limits)
+        -l LIST   File with number of sites to download
 
-		either site or list must be defined
+        either site or list must be defined
 EOF
 }
 
